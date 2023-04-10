9c7463e5404c38f8c88418ef26b603b2421ec11b
harry <harry.nguyen.2016@gmail.com>
2023-04-10 14:25:53 -0400

    first commit

---
+++ Unit_2_Room_Calculator_Lab/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Formats.Asn1;
+
+public class Program
+{
+    static void Main()
+    {
+        decimal length = 0;
+        decimal width = 0;
+        decimal height = 0;
+        Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+        Console.Write("Enter length: ");
+        length = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Enter width: ");
+        width = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Enter height: ");
+        height = decimal.Parse(Console.ReadLine());
+
+        decimal area = length * width;
+        decimal perimeter = 2 * (length + width);
+        decimal volume = length * width * height;
+        decimal surfaceArea = 2*(length * width + width * height + length * height);
+
+        Console.WriteLine($"Area: {area}");
+        Console.WriteLine($"Perimeter: {perimeter}");
+        Console.WriteLine($"Volume: {volume}");
+        Console.WriteLine($"Surface area: {surfaceArea}");
+
+        string roomSize = "";
+        if (area <= 250)
+            roomSize = "small";
+        else if (area > 250 && area < 650)
+            roomSize = "medium";
+        else if (area >= 650)
+            roomSize = "large";
+
+        Console.WriteLine($"Calculated: This is a {roomSize}-sized room.");
+        Console.WriteLine("Thank you for using the Room Detail Generator!");
+
+    }
+}
